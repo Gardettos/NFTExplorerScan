54b0f89db754e51a75aaa93ce9d3167b57a74ca3
Gardettos <77548895+Gardettos@users.noreply.github.com>
2022-02-15 15:10:32 -0600

    Initial Commit

---
+++ Models/CollectionStats.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace NFTExplorerScan.Models
+{
+    public class CollectionStats
+    {
+        private string _collectionName;
+        private string _items;
+        private string _floorPrice;
+        private string _totalVolume;
+        private DateTime _scrapedTime;
+
+        public CollectionStats(string collectionName, string items, string floorPrice, string totalVolume, DateTime scrapedTime)
+        {
+            _collectionName = collectionName;
+            _items = items.Replace("items", "");
+            _items.Trim();
+            _floorPrice = floorPrice.Replace("floor price", "");
+            _floorPrice.Trim();
+            _totalVolume = totalVolume.Replace("total volume", "");
+            _totalVolume.Trim();
+            _scrapedTime = scrapedTime;
+        }
+
+        public string CollectionName
+        {
+            get => _collectionName;
+        }
+
+        public string Items
+        {
+            get => _items;
+        }
+
+        public string FloorPrice
+        {
+            get => _floorPrice;
+        }
+
+        public string TotalVolume
+        {
+            get => _totalVolume;
+        }
+
+        public DateTime ScrapeTime
+        {
+            get => _scrapedTime;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Edge;
+using HtmlAgilityPack;
+using NFTExplorerScan.Models;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace NFTExplorerScan
+{
+    class Program
+    {
+        static string[] urls = new string[] { "yieldlings", "the-parliament-of-aowls", "trinley-s-many-figures" };
+
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new EdgeDriver();
+            //IWebDriver driver = new SafariDriver();
+
+            List<CollectionStats> ScrapedStats = new List<CollectionStats>();
+
+
+            for (int i = 0; i < urls.Length; i++)
+            {
+
+                string innerHtml = System.String.Empty;
+                IWebElement collections;
+
+                try
+                {
+                    //maybe see if theres an open check before navigating
+                    driver.Navigate().GoToUrl(string.Format("{0}/{1}", "https://www.nftexplorer.app/collection", urls[i]));
+                    Thread.Sleep(30000); //Wait 30 seconds to give javascript time to load
+                    collections = driver.FindElement(By.XPath(".//div[@class='flex-wrap justify-content-center list-group list-group-horizontal']"));
+                    innerHtml = collections.GetAttribute("innerHTML");
+                }
+                catch (Exception e) { }
+
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(innerHtml);
+
+                ScrapedStats.Add(new CollectionStats(
+                    collectionName: urls[i],
+                    items: doc.DocumentNode.SelectNodes("div")[0].InnerText,
+                    floorPrice: doc.DocumentNode.SelectNodes("div")[1].InnerText,
+                    totalVolume: doc.DocumentNode.SelectNodes("div")[2].InnerText,
+                    scrapedTime: DateTime.Now)
+                );
+
+            }
+
+            driver.Quit();
+
+            foreach (CollectionStats c in ScrapedStats)
+                Console.WriteLine("{0}{1}{2}{3}{4}", c.CollectionName, c.Items, c.FloorPrice, c.TotalVolume, c.ScrapeTime.ToString());
+
+        }
+    }
+}
